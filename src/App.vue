d638831f07ab9a41d9a1d6c9d42a4b125eb16ced
Ederson Ferreira <ederson.dev@gmail.com>
2026-03-16 15:43:44 -0300

    refactor: update App.vue to use AdminLayout and clean up main.js imports

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup></script>
+<script setup>
+import AdminLayout from './components/admin/AdminLayout.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <AdminLayout />
 </template>
 
 <style scoped></style>
