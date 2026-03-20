d613daeba2140034dde55517478efb338e5cf7e4
Ederson Ferreira <ederson.dev@gmail.com>
2026-03-19 21:57:27 -0300

    feat: add main CSS styles for dark mode and update import path in main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css';
 
 import App from './App.vue'
 import router from './router'
-import './assets/main.css'
+import './assets/styles/main.css'
 
 const app = createApp(App)
 
